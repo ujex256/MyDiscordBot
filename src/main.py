ac5d5ab70af66266e742075a2d74395a2835db03
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-12-20 21:39:55 +0900

    iroiro

--- src/main.py
+++ src/main.py
@@ -1,38 +1,53 @@
 from discord.ext import commands
+from discord import app_commands
 import discord
 import dotenv
 
+from random import randint
+from datetime import datetime
+from datetime import timezone
 from os import getenv
 
 
 intents = discord.Intents.default()
-intents.message_content = True  # メッセージの内容を取得する権限
+intents.message_content = True
 
 
-# Botをインスタンス化
 bot = commands.Bot(
-    command_prefix="$",  # $コマンド名でコマンドを実行できるようになる
-    case_insensitive=True,  # コマンドの大文字小文字を区別しない ($hello も $Hello も同じ!)
-    intents=intents  # 権限を設定
+    command_prefix="!",
+    case_insensitive=True,
+    intents=intents
 )
 
 
 @bot.event
 async def on_ready():
     print("Bot is ready!")
+    await bot.add_cog(CommonComands(bot))
     await bot.tree.sync()
     print("Synced")
 
 
-@bot.tree.command(name="ping", description="Ping!!!")
-async def ping(ctx: discord.Interaction):
-    sec = round(bot.latency * 1000)
-    embed = discord.Embed(
-        title="Pong!!",
-        description=str(sec) + "ms",
-        color=discord.Colour.blue()
-    )
-    await ctx.response.send_message(embed=embed)
+class CommonComands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="ping", description="Ping!!!")
+    async def ping(self, ctx: discord.Interaction):
+        sec = round(bot.latency * 1000)
+        embed = discord.Embed(
+            title="Pong!!",
+            description=str(sec) + "ms",
+            color=discord.Colour.blue()
+        )
+
+        value = datetime.now(timezone.utc) - ctx.created_at
+        embed.add_field(name="nowDate-timestamp", value=str(value.microseconds / 1000) + "ms")
+        await ctx.response.send_message(embed=embed)
+
+    @app_commands.command(name="random", description="random.randint()")
+    async def random(self, ctx: discord.Interaction):
+        await ctx.response.send_message(randint(1, 100))
 
 
 if __name__ == "__main__":
