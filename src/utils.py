9a7358c4cfc6f97e154feb030b589a208ed66d56
fffena <132272963+fffena@users.noreply.github.com>
2023-12-30 13:13:36 +0000

    fix: 消し忘れ

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,6 @@ from discord import Color
 def generate_secure_id(n: int) -> int:
     min_value = 10 ** (n - 1)
     max_value = (10 ** n) - 1
-    print(max_value)
     secure_id = secrets.randbelow(max_value - min_value + 1) + min_value
     return secure_id
 
