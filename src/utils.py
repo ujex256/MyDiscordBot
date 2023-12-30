e0651a62f33da6fad876061d1ec86935d575e7b4
fffena <132272963+fffena@users.noreply.github.com>
2023-12-30 09:40:50 +0000

    add: 使うかは不明

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import secrets
 
+from discord import Color
+
 
 def generate_secure_id(n: int) -> int:
     min_value = 10 ** (n - 1)
@@ -7,3 +9,8 @@ def generate_secure_id(n: int) -> int:
     print(max_value)
     secure_id = secrets.randbelow(max_value - min_value + 1) + min_value
     return secure_id
+
+
+class EmbedColors:
+    SUCCESS = Color.blue()
+    FAILED = Color.red()
