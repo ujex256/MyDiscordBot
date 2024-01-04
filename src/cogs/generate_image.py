843cefa4781616e685bab7182bfacd6e0333dd6f
ujex256 <105550500+ujex256@users.noreply.github.com>
2024-01-04 14:31:35 +0900

    feat: 完成した(コミット忘れてた)

--- src/cogs/generate_image.py
+++ src/cogs/generate_image.py
@@ -1,6 +1,8 @@
 import uuid
+import logging
 from io import BytesIO
 
+import coloredlogs
 import webuiapi
 import discord
 from discord import app_commands as ac
@@ -10,6 +12,38 @@ from utils import stable_diffusion as sd
 
 
 PORT = 7861
+coloredlogs.install()
+logger = logging.getLogger(__name__)
+
+_api = webuiapi.WebUIApi(port=PORT)
+logger.info("Initialized sd-api")
+_sd_models = sd.ModelsAPI(_api)
+logger.debug("Initialized sd.ModelsAPI")
+_sd_models.set_current_options(sd.Defaults.to_options())
+logger.info("Applied sd-options")
+
+
+class AutoCompletions:
+    @classmethod
+    async def model(cls, ctx: discord.Interaction, inputted: str):
+        models = _sd_models.get_models()
+        return await cls._candidate(inputted, models)
+
+    @classmethod
+    async def vae(cls, ctx: discord.Interaction, inputted: str):
+        vaes = _sd_models.get_vaes()
+        return await cls._candidate(inputted, vaes)
+
+    @classmethod
+    async def sampler(cls, ctx: discord.Interaction, inputted: str):
+        samplers = [i.value for i in sd.Samplers]
+        return await cls._candidate(inputted, samplers)
+
+    @staticmethod
+    async def _candidate(text: str, candidates: list):
+        if text == "":
+            return [ac.Choice(name=i, value=i) for i in candidates]
+        return [ac.Choice(name=i, value=i) for i in candidates if text.lower() in i.lower()]
 
 
 class SDCog(ac.Group):
@@ -18,27 +52,63 @@ class SDCog(ac.Group):
     Args:
         bot (discord.ext.commands.Bot): Bot instance
     """
+    DEFAULT_OPTIONS = {
+        "sd_model_checkpoint": "Anything-v4.5-pruned-mergedVae"
+    }
 
     def __init__(self, bot: Bot, **kwargs) -> None:
         super().__init__(**kwargs)
         self.bot = bot
-        self.api = webuiapi.WebUIApi(port=PORT, sampler=sd.Samplers.DPM_2M_KARRAS)
 
     @ac.command(name="txt2img")
+    @ac.autocomplete(
+        model=AutoCompletions.model,
+        vae=AutoCompletions.vae,
+        sampler=AutoCompletions.sampler
+    )
     async def generate(
         self,
         interaction: discord.Interaction,
         prompt: str,
         negative_prompt: str = "",
+        model: str | None = None,
+        vae: str | None = None,
+        sampler: str | None = None,
         height: int = 512,
         width: int = 512,
         steps: int = 20,
         seed: int = -1,
         cfg_scale: float = 7.0,
+        ignore_easynegative: bool = False
     ):
-        a = {
+
+        option = sd.Defaults.to_options()
+
+        embed = None
+        if model and model not in _sd_models.get_models():
+            embed = discord.Embed(title="エラー！", description="モデルがありません", color=discord.Color.red())
+        elif vae and vae not in _sd_models.get_vaes():
+            embed = discord.Embed(title="エラー！", description="VAEがありません", color=discord.Color.red())
+        elif model:
+            option.model = model
+        elif vae:
+            option.vae = vae
+
+        if embed:
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+            return
+
+        embed = discord.Embed(title="生成中...", description="", color=discord.Color.blue())
+        await interaction.response.send_message(embed=embed)
+
+        _sd_models.set_current_options(option)
+
+        if sampler is None:
+            sampler = sd.Defaults.SAMPLER  # dpm++ 2m karras
+        params = {
             "prompt": prompt,
             "negative_prompt": negative_prompt,
+            "sampler_name": sampler,
             "height": height,
             "width": width,
             "steps": steps,
@@ -46,17 +116,18 @@ class SDCog(ac.Group):
             "cfg_scale": cfg_scale,
             "use_async": True,
         }
-        embed = discord.Embed(title="生成中...", description="")
-        await interaction.response.send_message(embed=embed)
-        img = await self.api.txt2img(**a)  # type: ignore
+        _negative = map(lambda x: x.replace(" ", ""), params["negative_prompt"].split(","))
+        if sd.Defaults.EMBEDDING not in _negative and not ignore_easynegative:
+            params["negative_prompt"] = sd.Defaults.EMBEDDING + "," + params["negative_prompt"]
+
+        img = await _api.txt2img(**params)  # type: ignore
 
         img_bytes = BytesIO()
         img.image.save(img_bytes, format="png")
         img_bytes.seek(0)  # ここ重要
         img_filename = str(uuid.uuid4()).replace("-", "") + ".png"
-        print(img_filename)
 
-        result = discord.Embed(title="生成完了")
+        result = discord.Embed(title="生成完了", color=discord.Color.blue())
         attach = discord.File(img_bytes, filename=img_filename)
         result.set_image(url=f"attachment://{img_filename}")
         await interaction.edit_original_response(embed=result, attachments=[attach])
--- src/utils/stable_diffusion.py
+++ src/utils/stable_diffusion.py
@@ -1,8 +1,11 @@
 import webuiapi
+from dataclasses import dataclass
+from discord import app_commands as ac
+from enum import Enum
 
 
 # stable-diffusion-webui V1.7.0のサンプラー
-class Samplers:
+class Samplers(Enum):
     DPM_2M_KARRAS = "DPM++ 2M Karras"
     DPM_SDE_KARRAS = "DPM++ SDE Karras"
     DPM_2M_SDE_EXPONENTIAL = "DPM++ 2M SDE Exponential"
@@ -35,28 +38,54 @@ class Samplers:
     UNIPC = "UniPC"
 
 
+@dataclass
+class Options:
+    model: str
+    vae: str
+
+
 class Defaults:
     MODEL = "Anything-v4.5-pruned-mergedVae"
     VAE = "clearvae_v23"
+    VAE_FULLNAME = "clearvae_v23.safetensors"
     EMBEDDING = "EasyNegative"
+    SAMPLER = Samplers.DPM_2M_KARRAS.value
+
+    @classmethod
+    def to_dict(cls):
+        return {
+            "sd_model_checkpoint": cls.MODEL,
+            "sd_vae": cls.VAE
+        }
+
+    @classmethod
+    def to_options(cls):
+        return Options(
+            model=cls.MODEL,
+            vae=cls.VAE
+        )
 
 
 class ModelsAPI:
     def __init__(self, api: webuiapi.WebUIApi) -> None:
         self.api = api
 
-    async def all_get(self):
-        await self.api.get_sd_models()
+    def get_current_options(self):
+        options = self.api.get_options()
+        return Options(
+            model=options["sd_model_checkpoint"],
+            vae=options["sd_vae"]
+        )
 
-    @property
+    def set_current_options(self, options: Options):
+        self.api.set_options({"sd_model_checkpoint": options.model, "sd_vae": options.vae})
+
     def get_models(self):
         return [i["model_name"] for i in self.api.get_sd_models()]
 
-    @property
-    def get_vae(self):
+    def get_vaes(self):
         return [i["model_name"].removesuffix(".safetensors").removesuffix(".vae.pt") for i in self.api.get_sd_vae()]
 
-    @property
     def get_embeddings(self):
         return list(self.api.get_embeddings()["loaded"].keys())
 
@@ -65,3 +94,7 @@ def _format_name(name: str, upper: bool = True) -> str:
     if upper:
         name = name.upper()
     return name.replace("", "_").replace("-", "_").replace("+", "")
+
+
+def make_choices(choices: list):
+    return [ac.Choice(name=i, value=i) for i in choices]
